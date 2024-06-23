11a36f853e1a437a9e89d9224fc6df4fb6fc13b3
julietarutar97 <julietarutar97@gmail.com>
2024-06-23 11:44:26 -0300

    Cree clase libro

---
+++ Js/libro.js
@@ -0,0 +1,10 @@
+
+class Libro{
+    // titulo (string), autor (string), añoPublicacion (número) y genero (string)
+    constructor(titulo, autor, añoPublicacion, genero){
+        this.titulo=titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.genero =genero;
+    }
+}
